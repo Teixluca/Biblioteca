9e76f5aeb854e0d127508dbcefc35edc3480ba8a
Aluno <lucas.alvesteixeira@outlook.com>
2024-11-29 21:34:00 -0300

    INICIO

--- App.js
+++ App.js
@@ -1,20 +1,142 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View, ScrollView } from 'react-native';
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.cabeçalho}>
+            <text>
+              BIBLIOTECA
+            </text>
+      </View>
+      
+
+
+      <ScrollView>
+        
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Outro livro'
+            color='red'
+            onPress={() => onMessage()} />
+        </View>
+
+      </ScrollView>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'right',
+    justifyContent: 'top',
   },
+
+  caixa: {
+    backgroundColor: "red",
+    padding: 15,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    marginVertical: 10
+  },
+
+  cabeçalho: {
+    flex: 1,
+    backgroundColor: 'lightred',
+    padding: 16,
+    textAlign: "center"
+  },
+
+  descrição: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8
+  },
+
+  buttonContainer: {
+    backgroundColor: "red",
+    padding: 15,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    marginVertical: 10
+
+  },
+
+
 });
